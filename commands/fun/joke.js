8605b6ae81af0c0490a228d9940594355a8fb532
Zmezmer <kyneff00@gmail.com>
2020-06-14 21:09:30 -0500

    Added joke command.

---
+++ commands/fun/joke.js
@@ -0,0 +1,30 @@
+const { prefix } = require('../../config.json');
+const axios = require('axios');
+const { MessageEmbed } = require('discord.js');
+const { blue } = require('../../colors.json');
+
+module.exports = {
+	config: {
+		name: 'joke',
+		usage: `${prefix}joke`,
+		category: 'fun',
+		description: 'Mighty Poro tells a random joke.',
+		accessableby: 'Verified',
+	},
+	run: async (bot, message, args) => {
+		const getJoke = async () => {
+            const response = await axios.get(
+                'https://official-joke-api.appspot.com/random_joke'
+            );
+            const joke = response.data;
+            return joke;
+        };
+        const jokeValue = await getJoke();
+        const embed = new MessageEmbed()
+                .setColor(blue)
+                .setThumbnail(message.guild.iconURL())
+				.setTitle('Jokes!')
+				.setDescription(`${jokeValue.setup}\n\n${jokeValue.punchline}`)
+			message.channel.send(embed);
+	},
+};
